63458410c674cda28f00a0c969f740a0ebe937f9
nikkalininskiy <148541665+nikkalininskiy@users.noreply.github.com>
2023-12-21 23:04:16 +0300

    Конечная версия программы

--- traingles.cpp
+++ traingles.cpp
@@ -4,10 +4,19 @@ using namespace std;
 int main() {
     SetConsoleCP(1251);
     SetConsoleOutputCP(1251);
-    
+
     cout << "Введите количество треугольников, которое хотите увидеть: ";
     int n;
     cin >> n;
+    do {
+
+        if (std::cin.fail() || n < 0) {
+            std::cin.clear();
+            std::cin.ignore(1000, '\n');
+            std::cout << "Ошибка: Введите n повторно." << std::endl;
+            std::cin >> n;
+        }
+    } while (std::cin.fail() || n < 0);
     int spaces = n;
     for (int i = 1; i <= n; ++i) {
         for (int t = 1; t <= n; ++t) {
